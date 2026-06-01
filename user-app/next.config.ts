bb761be875a3c1fcae717cd2cf104519af45a12f
Amit kumar <amitakhil001@gmail.com>
2026-06-01 12:27:48 +0530

    Fix Vercel turbopack deployment error

--- user-app/next.config.ts
+++ user-app/next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: "c:/Users/amita/myprojects/placement",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
